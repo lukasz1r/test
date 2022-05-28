03e50249742aebe669712249e328a305fe83ca24
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-05-28 21:41:55 +0200

    all

--- Lotnisko.java
+++ Lotnisko.java
@@ -1,3 +1,23 @@
 public class Lotnisko {
     
+    private String nazwa;
+    private int x, y;
+
+    Lotnisko(String nazwa, int x, int y){
+        this.nazwa = nazwa;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return 0;
+    }
+
+    public int getY(){
+        return 0;
+    }
+
+
+
+
 }
--- Maszyna.java
+++ Maszyna.java
@@ -1,3 +1,15 @@
 public class Maszyna {
     
+    public int getMiejsca(){
+        return 0;
+    }
+
+    public int getZasieg(){
+        return 0;
+    }
+
+    public String getNazwa(){
+        return " brak ";
+    }
+
 }
