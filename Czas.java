85deef54f0a859dd5aa6ceaecf63f557b819b732
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-05-29 00:35:51 +0200

    update klilku klas

--- Czas.java
+++ Czas.java
@@ -1,3 +1,13 @@
 public class Czas {
-    
+    private int godzina, minuta;
+
+    public int getGodzina(){
+        return 0;
+    }
+
+    public int getMinuta(){
+        return 0;
+    }
+
+
 }
