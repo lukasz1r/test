3e3629d548eb78d7f71742fd3499c4a234a35e21
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-05-29 00:28:53 +0200

    update klilku klas

--- Trasa.java
+++ Trasa.java
@@ -1,3 +1,35 @@
 public class Trasa {
     
+    private Lotnisko poczatek;
+    private Lotnisko koniec;
+    private String przypisanaMaszyna;
+    private int iloscMiejsc;
+    private int czestotliwosc;
+    private float cena;
+    private Czas czasOdlotu;
+
+    Trasa(Lotnisko a, Lotnisko b, int czestotliwosc, Czas czas){
+        poczatek = a;
+        koniec = b;
+        this.czestotliwosc = czestotliwosc;
+        czasOdlotu = czas;
+    }
+
+    public void zakup(){
+
+    }
+
+    public int getIloscMiejsc(){
+        return iloscMiejsc;
+    }
+
+    public float getCena(){
+        return cena;
+    }
+
+    public Czas getCzas(){
+        return czasOdlotu;
+    }
+
+
 }
