4efc0ebbc03666139999ea338506d8c576525d07
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-05-29 15:43:09 +0200

    all

--- Klient.java
+++ Klient.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klient {
     
+    private String imie, nazwisko, PESEL;
+
+    List<Rezerwacja> wykupioneLoty = new ArrayList<Rezerwacja>();
+
+    Klient(String imie, String nazwisko, String PESEL){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.PESEL = PESEL;
+    }
+
+    public void rezerwujLot(){
+
+    }
 }
--- Posrednik.java
+++ Posrednik.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Posrednik {
-    
+
+    private String nazwa;
+
+    List<Rezerwacja> wykupioneLoty = new ArrayList<Rezerwacja>();
+
+    Posrednik(String nazwa){
+        this.nazwa = nazwa;
+    }
+
+    public void rezerwujBilety(){
+        
+    }
 }
+
+
