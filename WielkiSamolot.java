85fac181d3aa1f77970fa454379b83b9f70a0533
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 19:05:57 +0200

    udokumentowane

--- WielkiSamolot.java
+++ WielkiSamolot.java
@@ -1,3 +1,13 @@
+/*
+ * <h3>Klasa WielkiSamolot definiuje największą dostępną maszynę.</h3>
+ * <p>Parametry</p>
+ * <ul>
+ *      <li>iloscMiejsc -> Maksymalna ilość miejsc jaką może pomieścić samolot</li>
+ *      <li>zasieg -> Maksymalny zasieg jaki samolot może obsłużyć</li>
+ *      <li>nazwa -> Nazwa samolotu, który przyjmie paramtery charakteryzujące WielkiSamolot</li>
+ * </ul>
+ */
+
 public class WielkiSamolot extends Maszyna{
     protected int iloscMiejsc = 400;
     protected int zasieg = 5000;
