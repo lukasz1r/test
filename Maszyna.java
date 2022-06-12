a8d943e0ea41561ea64243a7c02a7f72b33abbd0
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 20:22:23 +0200

    udokumentowane

--- Maszyna.java
+++ Maszyna.java
@@ -1,16 +1,23 @@
+/*
+ * <h3>Klasa Maszyna jest klasą abstrakcyjną, z której dziedziczą klasy samolotów.</h3>
+ * <p>Usprawnia ona zarządzanie samolotami</p>
+ */
 public abstract class Maszyna {
     
     protected int iloscMiejsc, zasieg;
     protected String nazwa;
 
+    //Metoda getMiejsca zwraca ilość miejsc jaką mięści maszyna
     public int getMiejsca(){
         return iloscMiejsc;
     }
 
+    //Metoda getZasieg zwraca zasięg maszyny
     public int getZasieg(){
         return zasieg;
     }
 
+    //Metoda getNazwa zwraca nazwę maszyny
     public String getNazwa(){
         return nazwa;
     }
