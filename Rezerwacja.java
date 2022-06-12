d705e116c6ef4397f5052d17b1a218488cb91d3d
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 20:11:01 +0200

    udokumentowane

--- Rezerwacja.java
+++ Rezerwacja.java
@@ -1,21 +1,35 @@
+/*
+ * <h3>Klasa rezerwacja</h>
+ * <p>Zawiera parametry: </p>
+ * <ul>
+ *      <li>cena - cena rezerwacji</li>
+ *      <li>trasa - zarezerwowana trasa</li>
+ *      <li>czas - czas wylotu</li>
+ * 
+ * </ul>
+ */
+
 public class Rezerwacja{
 
     private double cena; 
     private Trasa trasa;
     private Czas czas;
 
+    //Konstruktor rezerwacji
     Rezerwacja(Trasa trasa){
         this.trasa = trasa;
         this.cena = trasa.getCena();
         this.czas = trasa.getCzas();   
     }
 
+    //Metoda getTrasa zwraca trasę
     public Trasa getTrasa(){
         for(Trasa i : BazaDanych.Trasy){
             if(i.equals(trasa)) return i;
         }
         return null;
     }
+    //Metoda toString wypisuje paramtery rezerwacji
     public String toString() {
         return trasa + " | " + cena + " | " + czas;
     }
