31403d1a3e1b36fd1e06fba622eb331c5fe755d7
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 19:07:05 +0200

    udokumentowane

--- SredniSamolot.java
+++ SredniSamolot.java
@@ -1,3 +1,13 @@
+/*
+ * <h3>Klasa SredniSamolot definiuje największą dostępną maszynę.</h3>
+ * <p>Parametry</p>
+ * <ul>
+ *      <li>iloscMiejsc -> Maksymalna ilość miejsc jaką może pomieścić samolot</li>
+ *      <li>zasieg -> Maksymalny zasieg jaki samolot może obsłużyć</li>
+ *      <li>nazwa -> Nazwa samolotu, który przyjmie paramtery charakteryzujące SredniSamolot</li>
+ * </ul>
+ */
+
 public class SredniSamolot extends Maszyna{
     protected int iloscMiejsc = 100;
     protected int zasieg = 3000;
