bab7d581304931ee60b040fd75a18d65d39b4585
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 21:02:15 +0200

    udokumentowane

--- Czas.java
+++ Czas.java
@@ -1,3 +1,12 @@
+/*
+ * <h3>Klasa Czas definiuje czas wylotu samolotów.</h3>
+ * <p>Jej parametry to:</p>
+ * <ul>
+ *      <li>godzina - godzina wylotu</li>
+ *      <li>minuta - minuta wylotu</li>
+ * </ul>
+ */
+//Konstruktor czasu wylotu
 public class Czas {
     private int godzina, minuta;
     Czas(int godzina, int minuta){
@@ -5,14 +14,17 @@ public class Czas {
         this.minuta=minuta;
     }
 
+    //Metoda getGodzina zwraca godzinę
     public int getGodzina(){
         return godzina;
     }
 
+    //Metoda getMinuta zwraca minutę
     public int getMinuta() {
         return minuta;
     }
 
+    //Metoda toString zwraca parametry czasu
     public String toString(){
         return godzina+":"+minuta;
     }
