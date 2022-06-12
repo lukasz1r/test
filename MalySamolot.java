8efce0fbaff2c43465ebffda1e997902a130833c
Łukasz Kaliszewicz <lukaso1r123@gmail.com>
2022-06-12 19:07:40 +0200

    udokumentowane

--- MalySamolot.java
+++ MalySamolot.java
@@ -1,3 +1,13 @@
+/*
+ * <h3>Klasa MalySamolot definiuje największą dostępną maszynę.</h3>
+ * <p>Parametry</p>
+ * <ul>
+ *      <li>iloscMiejsc -> Maksymalna ilość miejsc jaką może pomieścić samolot</li>
+ *      <li>zasieg -> Maksymalny zasieg jaki samolot może obsłużyć</li>
+ *      <li>nazwa -> Nazwa samolotu, który przyjmie paramtery charakteryzujące MalySamolot</li>
+ * </ul>
+ */
+
 public class MalySamolot extends Maszyna{
     
     protected int iloscMiejsc=20;
